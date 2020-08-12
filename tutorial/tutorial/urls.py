a29bde2946d6d7fd96f2518adf964e6560499df9
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-08-12 17:03:12 -0300

    Add authentication and permissions

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.conf.urls import include
 
 urlpatterns = [
     path('', include('snippets.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
